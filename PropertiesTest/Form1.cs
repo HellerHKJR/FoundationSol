6f7bc43fa435fefdfa61e40e68bf8bc01aba64a5
jryou <jryu@hellerindustries.co.kr>
2018-11-16 15:46:20 +0900

    regular commit

--- PropertiesTest/Form1.cs
+++ PropertiesTest/Form1.cs
@@ -14,6 +14,21 @@ namespace PropertiesTest
         public Form1()
         {
             InitializeComponent();
+
+            label1.Text = Properties.Settings.Default.TestV1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.TestV1 = "RRR";
+            Properties.Settings.Default.Save();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //reload
+            label1.Text = Properties.Settings.Default.TestV1;
         }
     }
 }
